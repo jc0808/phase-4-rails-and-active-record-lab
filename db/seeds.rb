1b7a65dee3bd0ea7558d85e29a77d5e024307f50
Joshua Campos <jdeveloper1203@gmail.com>
2022-11-04 14:04:23 -0400

    commit tree 6d1a286c8cbd10b7aa319c727178d50079f02d84

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Student.create([{first_name: Faker::Name.first_name, last_name: Faker::Name.last_name}, {first_name: Faker::Name.first_name, last_name: Faker::Name.last_name}, {first_name: Faker::Name.first_name, last_name: Faker::Name.last_name}])
